a4947f3fa69a0949032a2b4f2f5f14517cd62889
Wanhar <wanhardaengmaro@gmail.com>
2023-06-24 22:57:59 +0800

    get user data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crowdfunding/user"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,11 +10,22 @@ import (
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connecting to database...")
+
+	var users []user.User
+
+	db.Find(&users)
+
+	length := len(users)
+	fmt.Println("Length of users", length)
+
+	for _, user := range users {
+		fmt.Println("User name", user.Name)
+	}
 }
