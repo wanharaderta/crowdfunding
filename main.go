49551d180d5301b64e6feb564791c6cecbe18ead
Wanhar <wanhardaengmaro@gmail.com>
2023-06-25 15:04:56 +0800

    structure file

--- main.go
+++ main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"crowdfunding/user"
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 )
 
 func main() {
+	//dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//
+	//if err != nil {
+	//	log.Fatal(err.Error())
+	//}
+	//
+	//fmt.Println("Connecting to database...")
+	//
+	//var users []user.User
+	//db.Find(&users)
+	//
+	//for _, user := range users {
+	//	fmt.Println("User name", user.Name)
+	//}
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -16,16 +39,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connecting to database...")
-
 	var users []user.User
-
 	db.Find(&users)
 
-	length := len(users)
-	fmt.Println("Length of users", length)
-
-	for _, user := range users {
-		fmt.Println("User name", user.Name)
-	}
+	c.JSON(http.StatusOK, users)
 }
